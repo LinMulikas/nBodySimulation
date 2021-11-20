11d40d11aaca279f6d857d32e9e060534c3cf885
mulikas <12012727@mail.sustech.edu.cn>
2021-11-20 21:00:48 +0800

    在algs4基础上写入了引力系统，但会出现碰撞有效性检测的bug

--- src/rigidSphere.java
+++ src/rigidSphere.java
@@ -1,44 +1,57 @@
+import javafx.scene.shape.Circle;
+
 import java.awt.*;
 
-public class rigidSphere{
-	final double radius;
-	final double mass;
-	final Color color;
-	
-	Vector position;
-	Vector force;
-	Vector velocity;
-	Vector acceleration;
-	
-	public rigidSphere(double size, double mass, Color color, Vector position){
-		this.radius = size;
-		this.mass = mass;
-		this.color = color;
-		this.position = position;
-	}
-	
-	public void setVelocity(Vector v){
-		this.velocity = v;
-	}
-	
-	public void setForce(Vector force){
-		this.force = force;
-	}
-	
-	public void setAcceleration(Vector acceleration){
-		this.acceleration = acceleration;
-	}
-	
-	public void setP(long x, long y){
-		this.position.x = x;
-		this.position.y = y;
-	}
-	
-	
-	
-	public void upgrade(double tick){
-		this.acceleration.x = this.force.x/mass;
-		this.acceleration.y = this.force.y/mass;
-		
-	}
+public class rigidSphere extends Circle {
+    final double radius;
+    final double mass;
+    final Color color;
+    private boolean trail = false;
+
+    Vector position;
+    Vector prePosition;
+    Vector force;
+    Vector velocity;
+    Vector acceleration;
+
+
+
+    public rigidSphere(double size, double mass, Color color, Vector position) {
+        this.radius = size;
+        this.mass = mass;
+        this.color = color;
+        this.position = position;
+    }
+
+    public void setVelocity(Vector v) {
+        this.velocity = v;
+    }
+
+    public void setForce(Vector force) {
+        this.force = force;
+    }
+
+    public void setAcceleration(Vector acceleration) {
+        this.acceleration = acceleration;
+    }
+
+    public void setP(long x, long y) {
+        this.position.x = x;
+        this.position.y = y;
+    }
+
+    public void calPosition(double tick){
+        this.prePosition.x = this.position.x;
+        this.prePosition.y = this.position.y;
+        this.position.x = this.position.x + this.velocity.x*tick + 0.5*this.acceleration.x*tick*tick;
+        this.position.y = this.position.y + this.velocity.y*tick + 0.5*this.acceleration.y*tick*tick;
+    }
+
+    public boolean isTrail() {
+        return trail;
+    }
+
+    public void setTrail(boolean trail) {
+        this.trail = trail;
+    }
 }
--- src/universe.java
+++ src/universe.java
@@ -1,28 +1,61 @@
-public class physic{
-	private double G = 6.67e-11;
-	
+import java.util.ArrayList;
+import javafx.*;
+
+public class universe {
+	double G = 6.67e-11;
+	double width;
+	double height;
+	double tick = 0.1;
+	boolean ShowGUI = false;
+
+	universe(double x, double y){
+		this.width = x;
+		this.height = y;
+	}
+
+	universe(double x, double y, double g){
+		this.width = x;
+		this.height = y;
+		this.G = g;
+	}
+
+	public boolean checkBallCollision(rigidSphere ball){
+		return false;
+	}
+
+	public boolean checkBoundaryCollision(rigidSphere ball){
+		ball.calPosition(this.tick);
+		return false;
+	}
+
+	ArrayList<rigidSphere> spheres = new ArrayList<>();
+
 	public Vector gravityTo(rigidSphere ball1, rigidSphere ball2){
 		double x = ball2.position.x - ball1.position.x;
-		double y = ball2.position.y - ball2.position.y;
+		double y = ball2.position.y - ball1.position.y;
 		double d = Vector.distance(ball1.position, ball2.position);
-		
+
 		double scalaForce = gravity(ball1.mass, ball2.mass, d);
-		
+
 		return new Vector(scalaForce*(x/d), scalaForce*(y/d));
 	}
-	
+
+	public void addSphere(rigidSphere ball){
+		this.spheres.add(ball);
+	}
+
 	public double gravity(double m1, double m2, double distance){
 		return (this.G*m1*m2)/(distance*distance);
 	}
-	
+
 	public double getG(){
 		return G;
 	}
-	
+
 	public void setG(double g){
 		G = g;
 	}
-	
+
 	/**
 	 * 计算 v 的变化量
 	 *
@@ -34,9 +67,9 @@ public class physic{
 	public Vector dv(Vector a, double tick){
 		return new Vector(a.x*tick, a.y*tick);
 	}
-	
+
 	public Vector dx(Vector velocity, Vector a, double tick){
 		return new Vector(velocity.x*tick + 0.5*a.x*tick*tick, velocity.y*tick + 0.5*a.y*tick*tick);
 	}
-	
+
 }
