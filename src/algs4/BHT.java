29b3ed52c8c3b02d9e2717ad8285712cfd1536ea
mulikas <12012727@mail.sustech.edu.cn>
2021-12-09 16:01:24 +0800

    CO1 没有处理时间迟，已经完成合并

---
+++ src/algs4/BHT.java
@@ -0,0 +1,137 @@
+package algs4;
+
+import nBody.Body;
+
+//改写BHTree
+public class BHT{
+    private Particle particle;
+    private Body body;     // body or aggregate body stored in this node
+    private Quad quad;     // square region that the tree represents
+    private BHT NW;     // tree representing northwest quadrant
+    private BHT NE;     // tree representing northeast quadrant
+    private BHT SW;     // tree representing southwest quadrant
+    private BHT SE;     // tree representing southeast quadrant
+
+    //Create and initialize a new bhtree. Initially, all nodes are null and will be filled by recursion
+    //Each BHTree represents a quadrant and a body that represents all bodies inside the quadrant
+    public BHT(Quad q){
+        this.quad = q;
+        this.particle = null;
+        this.NW = null;
+        this.NE = null;
+        this.SW = null;
+        this.SE = null;
+    }
+
+    //If all nodes of the BHTree are null, then the quadrant represents a single body and it is "external"
+    public Boolean isExternal(){
+        if(NW == null && NE == null && SW == null && SE == null) return true;
+        else return false;
+    }
+
+    //We have to populate the tree with bodies. We start at the current tree and recursively travel through the branches
+    public void insert(Particle b){
+        //If there's not a body there already, put the body there.
+        if(this.particle == null){
+            this.particle = b;
+        }
+        //If there's already a body there, but it's not an external node
+        //combine the two bodies and figure out which quadrant of the
+        //tree it should be located in. Then recursively update the nodes below it.
+        else if(!this.isExternal()){
+            this.particle = b.centroid(this.particle, b);
+
+            Quad northwest = this.quad.NW();
+            if(b.in(northwest)){
+                if(this.NW == null){
+                    this.NW = new BHT(northwest);
+                }
+                NW.insert(b);
+            } else{
+                Quad northeast = this.quad.NE();
+                if(b.in(northeast)){
+                    if(this.NE == null){
+                        this.NE = new BHT(northeast);
+                    }
+                    NE.insert(b);
+                } else{
+                    Quad southeast = this.quad.SE();
+                    if(b.in(southeast)){
+                        if(this.SE == null){
+                            this.SE = new BHT(southeast);
+                        }
+                        SE.insert(b);
+                    } else{
+                        Quad southwest = this.quad.SW();
+                        if(this.SW == null){
+                            this.SW = new BHT(southwest);
+                        }
+                        SW.insert(b);
+                    }
+                }
+            }
+        }
+        //If the node is external and contains another body, create BHTrees
+        //where the bodies should go, update the node, and end
+        //(do not do anything recursively)
+        else if(this.isExternal()){
+            Particle c = this.particle;
+            Quad northwest = this.quad.NW();
+            if(c.in(northwest)){
+                if(this.NW == null){
+                    this.NW = new BHT(northwest);
+                }
+                NW.insert(c);
+            } else{
+                Quad northeast = this.quad.NE();
+                if(c.in(northeast)){
+                    if(this.NE == null){
+                        this.NE = new BHT(northeast);
+                    }
+                    NE.insert(c);
+                } else{
+                    Quad southeast = this.quad.SE();
+                    if(c.in(southeast)){
+                        if(this.SE == null){
+                            this.SE = new BHT(southeast);
+                        }
+                        SE.insert(c);
+                    } else{
+                        Quad southwest = this.quad.SW();
+                        //测试用 避免飞出边界时RE
+                        if(!c.in(southwest)) return;
+
+                        if(this.SW == null){
+                            this.SW = new BHT(southwest);
+                        }
+                        SW.insert(c);
+                    }
+                }
+            }
+            this.insert(b);
+        }
+    }
+
+    //用四叉树的方法近似计算施加在b上的所有力
+    //Start at the main node of the tree. Then, recursively go each branch
+    //Until either we reach an external node or we reach a node that is sufficiently
+    //far away that the external nodes would not matter much.
+    public void updateForce(Particle b, double g){
+        if(this.isExternal()){
+            if(this.particle != b) b.addForceTo(this.particle, g);
+        } else if(this.quad.length() / (this.particle.distanceTo(b)) < 2){
+            b.addForceTo(this.particle, g);
+        } else{
+            if(this.NW != null) this.NW.updateForce(b, g);
+            if(this.SW != null) this.SW.updateForce(b, g);
+            if(this.SE != null) this.SE.updateForce(b, g);
+            if(this.NE != null) this.NE.updateForce(b, g);
+        }
+    }
+
+    // convert to string representation for output
+    public String toString(){
+        if(NE != null || NW != null || SW != null || SE != null) return "*" + body + "\n" + NW + NE + SW + SE;
+        else return " " + body + "\n";
+    }
+}
--- src/algs4/Particle.java
+++ src/algs4/Particle.java
@@ -1,12 +1,4 @@
-package algs4; /******************************************************************************
- *  Compilation:  javac algs4.Particle.java
- *  Execution:    none
- *  Dependencies: StdDraw.java
- *
- *  A particle moving in the unit box with a given position, velocity,
- *  radius, and mass.
- *
- ******************************************************************************/
+package algs4;
 
 import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdRandom;
@@ -28,320 +20,279 @@ import java.awt.Color;
  * @author Kevin Wayne
  */
 public class Particle{
-	private static final double INFINITY = Double.POSITIVE_INFINITY;
-	
-	private double rx, ry;        // position
-	private double vx, vy;        // velocity
-	private double fx, fy;
-	private double ax, ay;
-	private int count;            // number of collisions so far
-	private final double radius;  // radius
-	private final double mass;    // mass
-	private final Color color;    // color
-	
-	public String toString(double width){
-		return width * this.rx + " " + width * this.ry + " " + width * this.vx + " " + width * this.vy + "\n";
-	}
-	
-	public String toString(){
-		return this.rx + " " + this.ry + " " + this.vx + " " + this.vy + "\n";
-	}
-	
-	/**
-	 * Initializes a particle with the specified position, velocity, radius, mass, and color.
-	 *
-	 * @param rx     <em>x</em>-coordinate of position
-	 * @param ry     <em>y</em>-coordinate of position
-	 * @param vx     <em>x</em>-coordinate of velocity
-	 * @param vy     <em>y</em>-coordinate of velocity
-	 * @param radius the radius
-	 * @param mass   the mass
-	 * @param color  the color
-	 */
-	public Particle(double rx, double ry, double vx, double vy, double radius, double mass, Color color){
-		this.vx = vx;
-		this.vy = vy;
-		this.rx = rx;
-		this.ry = ry;
-		this.radius = radius;
-		this.mass = mass;
-		this.color = color;
-	}
-	
-	public double sqrtDistanceTo(Particle particle){
-		return Math.sqrt((this.rx - particle.rx) * (this.rx - particle.rx) + (this.ry - particle.ry) * (this.ry - particle.ry));
-	}
-	
-	public double distanceTo(Particle particle){
-		return (this.rx - particle.rx) * (this.rx - particle.rx) + (this.ry - particle.ry) * (this.ry - particle.ry);
-	}
-	
-	public void addForceTo(Particle particle, double G){
-		double r = sqrtDistanceTo(particle);
-		double netForce = G * this.mass * particle.mass / (r * r);
-		this.fx += netForce * (particle.rx - this.rx) / (this.distanceTo(particle));
-		this.fy += netForce * (particle.ry - this.ry) / (this.distanceTo(particle));
-	}
-	
-	public void calNetForce(Particle[] particles, double G){
-		this.fx = this.calNetForce_x(particles, G);
-		this.fy = this.calNetForce_y(particles, G);
-	}
-	
-	private double calNetForce_x(Particle[] particles, double G){
-		double netF_x = 0;
-		for(Particle p : particles){
-			if(p != this){
-				if(this.distanceTo(p) > (this.radius + p.radius)){
-					netF_x += this.getNetForceTo(p, G) * (p.rx - this.rx) / (this.distanceTo(p));
-				}
-			}
-		}
-		return netF_x;
-	}
-	
-	private double calNetForce_y(Particle[] particles, double G){
-		double netF_y = 0;
-		for(Particle p : particles){
-			if(p != this){
-				if(this.distanceTo(p) > (this.radius + p.radius)){
-					netF_y += this.getNetForceTo(p, G) * (p.ry - this.ry) / (this.distanceTo(p));
-					
-				}
-			}
-		}
-		return netF_y;
-	}
-	
-	public double getNetForceTo(Particle particle, double G){
-		double r = sqrtDistanceTo(particle);
-		return G * this.mass * particle.mass / (r * r);
-	}
-	
-	
-	/**
-	 * Initializes a particle with a random position and velocity.
-	 * The position is uniform in the unit box; the velocity in
-	 * either direciton is chosen uniformly at random.
-	 */
-	public Particle(double width){
-		rx = StdRandom.uniform(0.0, width);
-		ry = StdRandom.uniform(0.0, width);
-		vx = StdRandom.uniform(-0.002, 0.02);
-		vy = StdRandom.uniform(-0.002, 0.02);
-		radius = 0.02;
-		mass = 1000000;
-		color = Color.BLACK;
-	}
-	
-	/**
-	 * Moves this particle in a straight line (based on its velocity)
-	 * for the specified amount of time.
-	 *
-	 * @param dt the amount of time
-	 */
-	public void move(double dt){
-		
-		
-		this.vx += this.ax * dt;
-		this.vy += this.ay * dt;
-		
-		rx += vx * dt;
-		ry += vy * dt;
-		
-	}
-	
-	public void move(double dt, Particle[] particles, double g){
-		this.vx += this.ax * dt;
-		this.vy += this.ay * dt;
-		
-		rx += vx * dt;
-		ry += vy * dt;
-		
-		this.calNetForce(particles, g);
-		this.calAcceleration();
-	}
-	
-	private class nextAction{
-		double totalTime;
-		double dt;
-		boolean collision = false;
-		
-	}
-	
-	public double getRx(){
-		return this.rx;
-	}
-	
-	public double getRy(){
-		return this.ry;
-	}
-	
-	public double getVx(){
-		return this.vx;
-	}
-	
-	public double getVy(){
-		return this.vy;
-	}
-	
-	public void moveDirectly(double dt){
-		if(dt < 0){
-			return;
-		}
-		this.vx += this.ax * dt;
-		this.vy += this.ay * dt;
-		
-		rx += vx * dt + 0.5 * this.ax * dt * dt;
-		ry += vy * dt + 0.5 * this.ay * dt * dt;
-	}
-	
-	public void calAcceleration(){
-		this.ax = this.fx / mass;
-		this.ay = this.fy / mass;
-	}
-	
-	/**
-	 * Draws this particle to standard draw.
-	 */
-	public void draw(){
-		StdDraw.setPenColor(color);
-		StdDraw.filledCircle(rx, ry, radius);
-	}
-	
-	
-	/**
-	 * Returns the number of collisions involving this particle with
-	 * vertical walls, horizontal walls, or other particles.
-	 * This is equal to the number of calls to {@link #bounceOff},
-	 * {@link #bounceOffVerticalWall}, and
-	 * {@link #bounceOffHorizontalWall}.
-	 *
-	 * @return the number of collisions involving this particle with
-	 * vertical walls, horizontal walls, or other particles
-	 */
-	public int count(){
-		return count;
-	}
-	
-	/**
-	 * Returns the amount of time for this particle to collide with the specified
-	 * particle, assuming no interening collisions.
-	 *
-	 * @param that the other particle
-	 * @return the amount of time for this particle to collide with the specified
-	 * particle, assuming no interening collisions;
-	 * {@code Double.POSITIVE_INFINITY} if the particles will not collide
-	 */
-	public double timeToHit(Particle that){
-		if(this == that) return INFINITY;
-		double dx = that.rx - this.rx;
-		double dy = that.ry - this.ry;
-		double dv_x = that.vx - this.vx;
-		double dv_y = that.vy - this.vy;
-		double dvdr = dx * dv_x + dy * dv_y;
-		if(dvdr > 0) return INFINITY;
-		double dvdv = dv_x * dv_x + dv_y * dv_y;
-		if(dvdv == 0) return INFINITY;
-		double drdr = dx * dx + dy * dy;
-		double sigma = this.radius + that.radius;
-		double d = (dvdr * dvdr) - dvdv * (drdr - sigma * sigma);
-		// if (drdr < sigma*sigma) StdOut.println("overlapping particles");
-		if(d < 0) return INFINITY;
-		return -(dvdr + Math.sqrt(d)) / dvdv;
-	}
-	
-	/**
-	 * Returns the amount of time for this particle to collide with a vertical
-	 * wall, assuming no interening collisions.
-	 *
-	 * @return the amount of time for this particle to collide with a vertical wall,
-	 * assuming no interening collisions;
-	 * {@code Double.POSITIVE_INFINITY} if the particle will not collide
-	 * with a vertical wall
-	 */
-	public double timeToHitVerticalWall(double width){
-		if(vx > 0) return (width - rx - radius) / vx;
-		else if(vx < 0) return (radius - rx) / vx;
-		else return INFINITY;
-	}
-	
-	/**
-	 * Returns the amount of time for this particle to collide with a horizontal
-	 * wall, assuming no interening collisions.
-	 *
-	 * @return the amount of time for this particle to collide with a horizontal wall,
-	 * assuming no interening collisions;
-	 * {@code Double.POSITIVE_INFINITY} if the particle will not collide
-	 * with a horizontal wall
-	 */
-	public double timeToHitHorizontalWall(double width){
-		if(vy > 0) return (width - ry - radius) / vy;
-		else if(vy < 0) return (radius - ry) / vy;
-		else return INFINITY;
-	}
-	
-	/**
-	 * Updates the velocities of this particle and the specified particle according
-	 * to the laws of elastic collision. Assumes that the particles are colliding
-	 * at this instant.
-	 *
-	 * @param that the other particle
-	 */
-	public void bounceOff(Particle that){
-		double dx = that.rx - this.rx;
-		double dy = that.ry - this.ry;
-		double dvx = that.vx - this.vx;
-		double dvy = that.vy - this.vy;
-		double dvdr = dx * dvx + dy * dvy;             // dv dot dr
-		double dist = this.radius + that.radius;   // distance between particle centers at collison
-		
-		// magnitude of normal force
-		double magnitude = 2 * this.mass * that.mass * dvdr / ((this.mass + that.mass) * dist);
-		
-		// normal force, and in x and y directions
-		double fx = magnitude * dx / dist;
-		double fy = magnitude * dy / dist;
-		
-		// update velocities according to normal force
-		this.vx += fx / this.mass;
-		this.vy += fy / this.mass;
-		that.vx -= fx / that.mass;
-		that.vy -= fy / that.mass;
-		
-		// update collision counts
-		this.count++;
-		that.count++;
-	}
-	
-	/**
-	 * Updates the velocity of this particle upon collision with a vertical
-	 * wall (by reflecting the velocity in the <em>x</em>-direction).
-	 * Assumes that the particle is colliding with a vertical wall at this instant.
-	 */
-	public void bounceOffVerticalWall(){
-		vx = -vx;
-		count++;
-	}
-	
-	/**
-	 * Updates the velocity of this particle upon collision with a horizontal
-	 * wall (by reflecting the velocity in the <em>y</em>-direction).
-	 * Assumes that the particle is colliding with a horizontal wall at this instant.
-	 */
-	public void bounceOffHorizontalWall(){
-		vy = -vy;
-		count++;
-	}
-	
-	/**
-	 * Returns the kinetic energy of this particle.
-	 * The kinetic energy is given by the formula 1/2 <em>m</em> <em>v</em><sup>2</sup>,
-	 * where <em>m</em> is the mass of this particle and <em>v</em> is its velocity.
-	 *
-	 * @return the kinetic energy of this particle
-	 */
-	public double kineticEnergy(){
-		return 0.5 * mass * (vx * vx + vy * vy);
-	}
+    private static final double INFINITY = Double.POSITIVE_INFINITY;
+
+    private double rx, ry;        // position
+    private double vx, vy;        // velocity
+    private double fx, fy;
+    private double ax, ay;
+    private int count;            // number of collisions so far
+    private final double radius;  // radius
+    private final double mass;    // mass
+    private final Color color;    // color
+
+    public String toString(double width){
+        return width * this.rx + " " + width * this.ry + " " + width * this.vx + " " + width * this.vy + "\n";
+    }
+
+    public String toString(){
+        return this.rx + " " + this.ry + " " + this.vx + " " + this.vy + "\n";
+    }
+
+    public Particle centroid(Particle a, Particle b){
+        double m = a.mass + b.mass;
+        return new Particle((a.rx * a.mass + b.rx * b.mass) / m, (a.ry * a.mass + b.ry * b.mass) / m, 0, 0, a.radius, m, a.color);
+    }
+
+    public boolean in(Quad q){
+        return q.contains(this.rx, this.ry);
+    }
+
+    public double v(){
+        return Math.sqrt(this.vx * this.vx + this.vy * this.vy);
+    }
+
+    /**
+     * Initializes a particle with the specified position, velocity, radius, mass, and color.
+     *
+     * @param rx     <em>x</em>-coordinate of position
+     * @param ry     <em>y</em>-coordinate of position
+     * @param vx     <em>x</em>-coordinate of velocity
+     * @param vy     <em>y</em>-coordinate of velocity
+     * @param radius the radius
+     * @param mass   the mass
+     * @param color  the color
+     */
+    public Particle(double rx, double ry, double vx, double vy, double radius, double mass, Color color){
+        this.vx = vx;
+        this.vy = vy;
+        this.rx = rx;
+        this.ry = ry;
+        this.radius = radius;
+        this.mass = mass;
+        this.color = color;
+    }
+
+    public double distanceTo(Particle particle){
+        return Math.sqrt((this.rx - particle.rx) * (this.rx - particle.rx) + (this.ry - particle.ry) * (this.ry - particle.ry));
+    }
+
+    public double squareDistanceTo(Particle particle){
+        return (this.rx - particle.rx) * (this.rx - particle.rx) + (this.ry - particle.ry) * (this.ry - particle.ry);
+    }
+
+    public void addForceTo(Particle particle, double G){
+        double r = distanceTo(particle);
+        if(this.distanceTo(particle) >= (this.radius + particle.radius)){
+            double netForce = G * this.mass * particle.mass / (r * r);
+            this.fx += netForce * (particle.rx - this.rx) / (r);
+            this.fy += netForce * (particle.ry - this.ry) / (r);
+        }
+    }
+
+
+    public void resetForce(){
+        this.fx = 0;
+        this.fy = 0;
+    }
+
+    /**
+     * Initializes a particle with a random position and velocity.
+     * The position is uniform in the unit box; the velocity in
+     * either direciton is chosen uniformly at random.
+     */
+    public Particle(double width){
+        rx = StdRandom.uniform(0.0, width);
+        ry = StdRandom.uniform(0.0, width);
+        vx = StdRandom.uniform(-0.002, 0.02);
+        vy = StdRandom.uniform(-0.002, 0.02);
+        radius = 0.02;
+        mass = 1000000;
+        color = Color.BLACK;
+    }
+
+    /**
+     * Moves this particle in a straight line (based on its velocity)
+     * for the specified amount of time.
+     *
+     * @param dt the amount of time
+     */
+    public void move(double dt){
+        this.vx += this.ax * dt;
+        this.vy += this.ay * dt;
+
+        rx += vx * dt;
+        ry += vy * dt;
+
+    }
+
+    public double getRx(){
+        return this.rx;
+    }
+
+    public double getRy(){
+        return this.ry;
+    }
+
+    public double getVx(){
+        return this.vx;
+    }
+
+    public double getVy(){
+        return this.vy;
+    }
+
+
+
+    public void calAcceleration(){
+        this.ax = this.fx / mass;
+        this.ay = this.fy / mass;
+    }
+
+    /**
+     * Draws this particle to standard draw.
+     */
+    public void draw(){
+        StdDraw.setPenColor(color);
+        StdDraw.filledCircle(rx, ry, radius);
+    }
+
+
+    /**
+     * Returns the number of collisions involving this particle with
+     * vertical walls, horizontal walls, or other particles.
+     * This is equal to the number of calls to {@link #bounceOff},
+     * {@link #bounceOffVerticalWall}, and
+     * {@link #bounceOffHorizontalWall}.
+     *
+     * @return the number of collisions involving this particle with
+     * vertical walls, horizontal walls, or other particles
+     */
+    public int count(){
+        return count;
+    }
+
+    /**
+     * Returns the amount of time for this particle to collide with the specified
+     * particle, assuming no interening collisions.
+     *
+     * @param that the other particle
+     * @return the amount of time for this particle to collide with the specified
+     * particle, assuming no interening collisions;
+     * {@code Double.POSITIVE_INFINITY} if the particles will not collide
+     */
+    public double timeToHit(Particle that){
+
+        double d_0 = Math.abs(this.distanceTo(that) - (this.radius + that.radius));
+        if(d_0 <= 1e-6){
+            return 0;
+        }
+
+        if(this == that) return INFINITY;
+        double dx = that.rx - this.rx;
+        double dy = that.ry - this.ry;
+        double dv_x = that.vx - this.vx;
+        double dv_y = that.vy - this.vy;
+        double dvdr = dx * dv_x + dy * dv_y;
+        if(dvdr > 0) return INFINITY;
+        double dvdv = dv_x * dv_x + dv_y * dv_y;
+        if(dvdv == 0) return INFINITY;
+        double drdr = dx * dx + dy * dy;
+        double sigma = this.radius + that.radius;
+        double d = (dvdr * dvdr) - dvdv * (drdr - sigma * sigma);
+        // if (drdr < sigma*sigma) StdOut.println("overlapping particles");
+        if(d < 0) return INFINITY;
+        return -(dvdr + Math.sqrt(d)) / dvdv;
+    }
+
+    /**
+     * Returns the amount of time for this particle to collide with a vertical
+     * wall, assuming no interening collisions.
+     *
+     * @return the amount of time for this particle to collide with a vertical wall,
+     * assuming no interening collisions;
+     * {@code Double.POSITIVE_INFINITY} if the particle will not collide
+     * with a vertical wall
+     */
+    public double timeToHitVerticalWall(double width){
+        if(vx > 0) return (width - rx - radius) / vx;
+        else if(vx < 0) return (radius - rx) / vx;
+        else return INFINITY;
+    }
+
+    /**
+     * Returns the amount of time for this particle to collide with a horizontal
+     * wall, assuming no interening collisions.
+     *
+     * @return the amount of time for this particle to collide with a horizontal wall,
+     * assuming no interening collisions;
+     * {@code Double.POSITIVE_INFINITY} if the particle will not collide
+     * with a horizontal wall
+     */
+    public double timeToHitHorizontalWall(double width){
+        if(vy > 0) return (width - ry - radius) / vy;
+        else if(vy < 0) return (radius - ry) / vy;
+        else return INFINITY;
+    }
+
+    /**
+     * Updates the velocities of this particle and the specified particle according
+     * to the laws of elastic collision. Assumes that the particles are colliding
+     * at this instant.
+     *
+     * @param that the other particle
+     */
+    public void bounceOff(Particle that){
+        double dx = that.rx - this.rx;
+        double dy = that.ry - this.ry;
+        double dvx = that.vx - this.vx;
+        double dvy = that.vy - this.vy;
+        double dvdr = dx * dvx + dy * dvy;             // dv dot dr
+        double dist = this.radius + that.radius;   // distance between particle centers at collison
+
+        // magnitude of normal force
+        double magnitude = 2 * this.mass * that.mass * dvdr / ((this.mass + that.mass) * dist);
+
+        // normal force, and in x and y directions
+        double fx = magnitude * dx / dist;
+        double fy = magnitude * dy / dist;
+
+        // update velocities according to normal force
+        this.vx += fx / this.mass;
+        this.vy += fy / this.mass;
+        that.vx -= fx / that.mass;
+        that.vy -= fy / that.mass;
+
+        // update collision counts
+        this.count++;
+        that.count++;
+    }
+
+    /**
+     * Updates the velocity of this particle upon collision with a vertical
+     * wall (by reflecting the velocity in the <em>x</em>-direction).
+     * Assumes that the particle is colliding with a vertical wall at this instant.
+     */
+    public void bounceOffVerticalWall(){
+        vx = -vx;
+        count++;
+    }
+
+    /**
+     * Updates the velocity of this particle upon collision with a horizontal
+     * wall (by reflecting the velocity in the <em>y</em>-direction).
+     * Assumes that the particle is colliding with a horizontal wall at this instant.
+     */
+    public void bounceOffHorizontalWall(){
+        vy = -vy;
+        count++;
+    }
+
+    public double kineticEnergy(){
+        return 0.5 * mass * (vx * vx + vy * vy);
+    }
+
+    public double getRadius(){
+        return this.radius;
+    }
 }
